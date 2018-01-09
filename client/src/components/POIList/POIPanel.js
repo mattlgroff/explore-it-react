8431cc652d87d1fc8b599481dea6e73ffd387cb2
Matt Groff <mattlgroff@gmail.com>
2018-01-08 19:38:21 -0800

    Added icons for 4 categories

--- client/src/components/POIList/POIPanel.js
+++ client/src/components/POIList/POIPanel.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import {ListGroup, ListGroupItem, Image} from 'react-bootstrap'
-import './index.css'
+import {ListGroup, ListGroupItem, Image} from 'react-bootstrap';
+import './index.css';
+import foodIcon from '../../assets/icons/foodanddrink.svg';
+import bathroomIcon from '../../assets/icons/bathroom.svg';
+import shoppingIcon from '../../assets/icons/shopping.svg';
+import attractionIcon from '../../assets/icons/attraction.svg';
 
 const POIPanel = (props) => {
   return (
     <ListGroup className='poiPanel'>
       {
-        props.pois.map((poi) =>
-          <ListGroupItem><Image className='poiImg' src={poi.img_url} circle />{poi.name}</ListGroupItem>
+        props.pois.map((poi) => {
+
+          let source;
+
+          switch(poi.category){
+            case 'Attraction':
+            source = attractionIcon;
+            break;
+
+            case 'Bathroom':
+            source = bathroomIcon;
+            break;
+
+            case 'Food and Drink':
+            source = foodIcon;
+            break;
+
+            case 'Shopping':
+            source = shoppingIcon;
+            break;
+
+            default:
+            break;
+          }
+
+          return (
+
+          <ListGroupItem><Image className='poiImg' src={source} />{poi.name}</ListGroupItem>
+          );
+        }
+
+        
         )
       }
     </ListGroup>
