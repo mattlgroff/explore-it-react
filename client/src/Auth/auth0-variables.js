562300368bb74b3ff5f677598fcc1f2be130a248
Matt Groff <mattlgroff@gmail.com>
2018-01-08 17:09:36 -0800

    We have code

---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'exploreit.auth0.com',
+  clientId: 'dOCYrqlfYFiXdnyrHNex-Lk4QyvO_CSr',
+  callbackUrl: 'http://localhost:3000/callback'
+}
--- server.js
+++ server.js
@@ -1,9 +1,44 @@
-const express = require("express");
+const express = require('express');
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const PORT = process.env.PORT || 7000;
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: 'https://dev-892472.oktapreview.com/oauth2/default',
+  assertClaims: {
+    aud: 'api://default',
+  },
+});
+
+function authenticationRequired(req, res, next) {
+  const authHeader = req.headers.authorization || '';
+  const match = authHeader.match(/Bearer (.+)/);
+
+  if (!match) {
+    return res.status(401).end();
+  }
+
+  const accessToken = match[1];
+
+  return oktaJwtVerifier.verifyAccessToken(accessToken)
+    .then((jwt) => {
+      req.jwt = jwt;
+      next();
+    })
+    .catch((err) => {
+      res.status(401).send(err.message);
+    });
+}
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+/**
+ * For local testing only!  Enables CORS for all domains
+ */
+app.use(cors());
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,15 +48,36 @@ app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
+/**
+ * An example route that requires a valid access token for authentication, it
+ * will echo the contents of the access token if the middleware successfully
+ * validated the token.
+ */
+app.get('/secure', authenticationRequired, (req, res) => {
+  res.json(req.jwt);
+});
+
+/**
+ * Another example route that requires a valid access token for authentication, and
+ * print some messages for the user if they are authenticated
+ */
+app.get('/api/messages', authenticationRequired, (req, res) => {
+  res.json([{
+    message: 'Hello, word!'
+  }]);
+});
+
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://localhost/belmont",
   {
     useMongoClient: true
   }
-);
+)
+.then(stuff => console.log(stuff))
+.catch(err => console.error(err));
 
 // Start the API server
 app.listen(PORT, function() {
