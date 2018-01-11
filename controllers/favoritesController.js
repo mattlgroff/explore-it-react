b20eae45831188d4aecfc302d2d1104f8c1acd91
Matt Groff <mattlgroff@gmail.com>
2018-01-10 20:35:38 -0800

    Auth working

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -2,24 +2,60 @@ const db = require("../models");
 
 // Defining methods for the favoritesController
 module.exports = {
-  findAll: function(req, res) {
+  findAllFavorites: function(req, res) {
+    let profile = req.body.profile;
+
     db.Favorites
-      .find(req.query)
-      .sort({ name: 1 })
+      .find(profile)
+      .sort({ list: 1 })
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .catch(err => {
+        console.error(err);
+        res.status(422).json(err)
+      });
   },
   addOne: function(req, res) {
+    console.log(req.body);
+    let profile = req.body.profile;
+    let list =  req.body.list;
+    console.log("Hit addOne controller option");
+    console.log("Profile: " + profile);
+    console.log("List: " + list);
+
     db.Favorites
       .create(req.body)
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .catch(err => {
+        console.error(err);
+        res.status(422).json(err)
+      });
   },
   remove: function(req, res) {
+    let profile = req.body.profile;
+    let list =  req.body.list;
+    console.log("Hit Remove controller option");
+    console.log("Profile: " + profile);
+    console.log("List: " + list);
+
     db.Favorites
-      .findById({ _id: req.params.id })
+      .find({ profile: req.body.profile, list: req.body.list })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .catch(err => {
+        console.error(err);
+        res.status(422).json(err)
+      });
+  },
+  isFavorite: function(req, res){
+    let profile = req.body.profile;
+    let list = req.body.list;
+
+    db.Favorites
+      .find({profile: profile, list: list})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.error(err);
+        res.status(422).json(err)
+      });
   }
 };
