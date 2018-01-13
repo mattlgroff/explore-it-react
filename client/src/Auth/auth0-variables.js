909b085dbeda1acf9bb6fa28c9d739b232dd78e3
Matt Groff <mattlgroff@gmail.com>
2018-01-13 13:41:01 -0800

    Unmerged client from server

--- client/src/Auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'exploreit.auth0.com',
-  clientId: 'dOCYrqlfYFiXdnyrHNex-Lk4QyvO_CSr',
-  callbackUrl: 'http://localhost:3000/callback'
-}
--- routes/poiRoutes.js
+++ routes/poiRoutes.js
@@ -2,17 +2,17 @@ const poiController = require("../controllers/poiController.js");
 
 module.exports = app => {
   //Sends JSON of every POI in our database
-  app.get("/poi/all", (req, res) => {
+  app.get("/belmont/poi/all", (req, res) => {
     poiController.findAll(req, res);
   });
 
   //Sends JSON of every Favorite POI in our database
-  app.post("/poi/findFavorites", (req, res) => {
+  app.post("/belmon/poi/findFavorites", (req, res) => {
     poiController.findAllFavorites(req, res);
   });
 
   //Sends JSON of one POI in our database
-  app.get("/poi/:id", (req, res) => {
+  app.get("/belmont/poi/:id", (req, res) => {
     poiController.findOne(req, res, req.params.id);
   });
 
