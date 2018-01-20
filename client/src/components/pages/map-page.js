d8ca97b8b026d905f3fee17ebc813c5e382c4982
Matt Groff <mattlgroff@gmail.com>
2018-01-20 13:02:07 -0800

    prop routes added. no more hardcode

--- client/src/components/pages/map-page.js
+++ client/src/components/pages/map-page.js
@@ -15,22 +15,14 @@ import "./map.css";
 class ExploreIt extends Component {
   constructor() {
     super()
-
-    //Replace these with Props if you can! Otherwise we can do two pages.
-    this.state = {
-      lat: 32.770844,
-      long: -117.2527572,
-      zoom: 18,
-      open: true
-    };
   }
 
 
   render() {
-    const position = [this.state.lat, this.state.long];
+    const position = [this.props.route.lat, this.props.route.long];
 
     return (
-      <Map zoomControl={false} center={position} zoom={this.state.zoom}>
+      <Map zoomControl={false} center={position} zoom={this.props.route.zoom}>
         <TileLayer
           attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>'
           url='https://api.mapbox.com/styles/v1/mattlgroff/cjcjws0xj18ea2sptc8iafsu5/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0dGxncm9mZiIsImEiOiJjamMzczFpNTExNWNmMnhwZjFvNGlpdnR4In0.y1gUOwBdSx6lhv_7TcmKJA'
