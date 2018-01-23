df0a63d868a161e40dfb7730cbc5b08039d4b6f4
innawu1 <innawuba@gmail.com>
2018-01-22 19:44:42 -0800

    Merged with JT

--- client/src/components/pages/home-page.js
+++ client/src/components/pages/home-page.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import "./home.css";
 
 
 class HomePage extends Component {
   render() {
     return (
-    <div className="container">
-        <Helmet>
-          <title>{this.props.route.name}</title>
-        </Helmet>
-		<div className="card">
-			<img className="card-img-top" src="https://exploreit.herokuapp.com/adrenzone.jpg" alt="Card image cap" />
+<div>
+	<Helmet>
+		<title>Explore Your World</title>
+	</Helmet>
+	<div className="jumbotron">
+		<h1 className="display-4">Explore It</h1>
+		<p className="lead">Maps As A Service</p>
+		<hr className="my-4" />
+		<p>Explore It is maps as a service. It's modular design allows any public area to have it's own map overlay.</p>
+	</div>
+	<div className = "container">
+	<div className = "row">
+		<div className="card mapCardStyle">
+			<img className="card-img-top" src="https://exploreit.herokuapp.com/belmont_park.png" alt="Belmont Park" />
+
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			<h5 className="card-title">Belmont Park</h5>
+			<p className="card-text">Belmont Park is a historic oceanfront amusement park located in the Mission Bay area of San Diego, California.</p>
 			</div>
 			<ul className="list-group list-group-flush">
-				<li className="list-group-item">Cras justo odio</li>
-				<li className="list-group-item">Dapibus ac facilisis in</li>
-				<li className="list-group-item">Vestibulum at eros</li>
+			<li className="list-group-item">Cras justo odio</li>
+			<li className="list-group-item">Dapibus ac facilisis in</li>
+			<li className="list-group-item">Vestibulum at eros</li>
 			</ul>
-			<div className="card-body">
-				<a href="#" className="card-link">Card link</a>
-				<a href="#" className="card-link">Another link</a>
-			</div>
 		</div>
+
+		<div className="card mapCardStyle">
+			<img className="card-img-top" src="https://exploreit.herokuapp.com/grossmont_college.png" alt="Grossmont College" />
+			<div className="card-body">
+			<h5 className="card-title">Grossmont College</h5>
+			<p className="card-text">Grossmont College is a community college located in El Cajon, California, in the county of San Diego.</p>
+			</div>
+			<ul className="list-group list-group-flush">
+			<li className="list-group-item">Cras justo odio</li>
+			<li className="list-group-item">Dapibus ac facilisis in</li>
+			<li className="list-group-item">Vestibulum at eros</li>
+			</ul>
+		</div>
+
 	</div>
+
+	
+
+	</div>
+</div>
+	
+
+
     );
   }
 }
